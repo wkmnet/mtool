b6028aec2627a0167618fd4439ee868b0ad36fde
wkmnet <wkmnet@hotmail.com>
2015-08-23 21:30:30 +0800

    测试例子

--- src/main/java/org/wkm/mtool/controller/TestController.java
+++ src/main/java/org/wkm/mtool/controller/TestController.java
@@ -10,6 +10,13 @@ import java.util.Map;
  */
 public class TestController extends Controller{
 
+
+    public void index(){
+        System.out.println("test...");
+        renderJson("{\"a\":\"b\"}");
+//        renderText("中华人民共和国！");
+    }
+
     public void test(){
         Map<String,String> m = new HashMap<String,String>();
         m.put("a","b");
