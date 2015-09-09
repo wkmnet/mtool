5b91bc5727dee6a7199917ae6518cc95c3000510
wukm <wkmnet@hotmail.com>
2015-09-09 21:09:47 +0800

    添加service

---
+++ src/main/java/org/wkm/mtool/service/CalendarService.java
@@ -0,0 +1,16 @@
+package org.wkm.mtool.service;
+
+/**
+ * Create with IntelliJ IDEA
+ * Project name : mtool
+ * Package name : org.wkm.mtool.service
+ * Author : Wukunmeng
+ * User : wkm
+ * Date : 15-9-9
+ * Time : 下午9:07
+ * 版权所有,侵权必究！
+ * To change this template use File | Settings | File Templates.
+ */
+public class CalendarService {
+
+}
