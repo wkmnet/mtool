18b95a1da80c1e4e083fe84f2537f87b43a86e22
wkmnet <wkmnet@hotmail.com>
2015-10-11 19:04:31 +0800

    修改日志输出方式

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -3,13 +3,11 @@ import static ch.qos.logback.classic.Level.DEBUG
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 
 StringBuilder p = new StringBuilder();
-p.append("<message time=\"%d{yyyy-MM-dd/HH:mm:ss}\" ");
-p.append("level=\"%-5level\" thread=\"%thread\" ");
-p.append("class=\"%class\" line=\"%line\">");
-p.append("%n");
+p.append("mtool %d{yyyy-MM-dd/HH:mm:ss} ");
+p.append("%-5level %thread ");
+p.append("%class %line 【");
 p.append("%msg");
-p.append("%n");
-p.append("</message>");
+p.append("】");
 p.append("%n");
 
 appender("CONSOLE", ConsoleAppender) {
