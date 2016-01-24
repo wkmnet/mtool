b354019d2fec4519bc8ca25b32aead21fd6c6963
wkm <wkmnet@hotmail.com>
2016-01-24 16:20:55 +0800

    增加特效css

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -6,5 +6,7 @@ var indexApp = angular.module("indexApp",[],function($httpProvider){
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 });
 indexApp.controller("loadMenuController",function($scope,$http){
-
+    $http.get('loadMenus').success(function(response){
+        $scope.menus = response;
+    });
 });
